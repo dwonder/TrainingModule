f6ca83b115e71c475a9a054ce9a31e2ea3b464f2
dwonder <dy_dayo@yahoo.com>
2025-09-03 10:17:00 +0000

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-        // Add this 'base' property for GitHub Pages
-        base: '/TrainingModule/', // 👈 Replace with your repository name
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-        define: {
-            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, '.'),
-            }
-        }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Replace 'your-repository-name' with the actual name of your repo
+  base: '/TrainingModule/', 
 });
